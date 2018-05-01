1f345077dbc3b7bf38a43524c65465e3d17f4400
Nour <nsaffour@gmail.com>
2018-05-01 21:44:49 +0200

    fixed word length error and memory leaks

--- lab 1/lab1.c
+++ lab 1/lab1.c
@@ -1,24 +1,35 @@
 
 #include <stdio.h>
 #include <time.h>
+#include <stdlib.h>
+
 void readWrite(char * inputfile, char * outputfile, int time) {
+  int wordCount = 0;
+  int cellsNum = 500000;
   FILE * fp = fopen(inputfile, "r");
   FILE * outputFP = fopen(outputfile, "w");
   if(fp == NULL || outputFP == NULL) {
     printf("There was an error reading from source file");
   }
-  char buff[10000][32];
+  char ** buff= (char**)malloc(sizeof(char*)* cellsNum);
   int temp = 0;
-  for(size_t i = 0; ;i++){
+  for(size_t i = 0;i < cellsNum ;i++){
+    wordCount++;
+    buff[i] = (char*)malloc(100);
     temp = fscanf(fp,"%s", buff[i]);
+    if(temp <= 0) {
+      free(buff[i]);
+      break;
+    }
     //printf(" %d ", temp);
-    fprintf(outputFP, "%s", buff[i]);
-    fprintf(outputFP,"\n");
-    if(temp <= 0) return;
+    fprintf(outputFP, "%s \n", buff[i]);
+    free(buff[i]);
   }
+  printf("the words total is %u \n",wordCount);
   fclose(fp);
   fclose(outputFP);
-
+  free(buff);
+  return;
   
   /*
   int i = 0;
@@ -41,13 +52,12 @@ int main(int argc, char * argv[]) {
     case 3: 
       inputFile = argv[1];
       outputFile = argv[2];
-      printf("got here");
-      //clock_t start, end;
-      //start = clock(); 
+      clock_t start, end;
+      start = clock(); 
       readWrite(inputFile, outputFile, 0);
-      //end = clock();
-      //int cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
-      //printf("The time in seconds is %d", cpu_time_used);
+      end = clock();
+      int cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
+      printf("The time in seconds is %d", cpu_time_used);
 
       break;
     case 4: 
